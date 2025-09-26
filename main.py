d68960cfcf0717e0045ffc0811f79a1b36c52b56
Maria Bashurova <xxxtacionxxx3@gmail.com>
2025-09-26 12:43:42 +0500

    2 ниче не работает

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ a = a.replace('//', '$')
 reg = r'[0-9]+'
 reg2 = r'[*|+|$|/|-]'
 # 132-4342*893//43
-num = []
+num1 = []
 num_1 = []
 num2 = []
 num_2 = []
 
 for i in finditer(reg, a):
-    num.append(int(i.group()))
+    num1.append(int(i.group()))
     num_1.append(int(i.group()))
 for j in finditer(reg2, a):
     num2.append(j.group())
@@ -21,19 +21,28 @@ for j in finditer(reg2, a):
 
 
 first = []
-for i in range(len(num2)):
+#for i in range(len(num2)):
+i = 0
+while '*' not in num2 or '$' not in num2 or '/' not in num2:
     if num2[i] in '*/$':
         if num2[i] == '*':
-            num_1[i] = num[i] * num[i+1]
-            num_1.remove(num[i+1])
+            num1[i] = num1[i] * num1[i+1]
+            num1.remove(num1[i+1])
+            #num2.remove(num2[i])
+            i += 1
         elif num2[i] == '/':
-            num_1[i] = num[i] / num[i + 1]
-            num_1.remove(num[i + 1])
+            num_1[i] = num1[i] / num1[i + 1]
+            num_1.remove(num1[i + 1])
+            num2.remove(num2[i])
+            i += 1
         elif num2[i] == '$':
-            print(i, num)
-            num_1[i] = num[i] // num[i + 1]
-            print(i, num)
-            num_1.remove(num[i + 1])
+            print(i, num1)
+            num_1[i] = num1[i] // num1[i + 1]
+            print(i, num1)
+            num_1.remove(num1[i + 1])
+            num2.remove(num2[i])
+            i += 1
+
 
 print(first)
 
