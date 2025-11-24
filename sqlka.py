a7ea46b8545cc31b3a4d2d9da64cd60af4c5ef90
lelooouch <118593450+lelooouch@users.noreply.github.com>
2025-11-24 18:02:06 +0500

    Update sqlka.py

--- sqlka.py
+++ sqlka.py
@@ -3,44 +3,64 @@ import sqlite3
 con = sqlite3.connect('db.sqlite')
 cur = con.cursor()
 
-query_1 = '''
-CREATE TABLE IF NOT EXISTS directors(
-    id INTEGER PRIMARY KEY,
-    full_name TEXT,
-    birth_year INTEGER
-    );
-    '''
+import sqlite3
 
-query_2 = '''
-CREATE TABLE IF NOT EXISTS video_products(
-    id INTEGER PRIMARY KEY,
-    title TEXT,
-    product_type TEXT,
-    release_year INTEGER
-    );
-    '''
-    
-query = '''
-    INSERT INTO video_products(id, title, product_type, release_year)
-    VALUES (1, 'Веселые мелодии', 'Мультсериал', 1930);
-    '''
+con = sqlite3.connect('db.sqlite')
+cur = con.cursor()
 
-# cur.execute(
-#     'INSERT INTO video_products VALUES(?, ?, ?, ?);',
-#     (2, 'Долгая прогулка', 'Фильм', 2025)
-# )
+# cur.executescript('''
+# DROP TABLE video_products2;
+# ''')
 
-# directors = [
-#     (1, 'Стивен Спилберг', 1946),
-#     (2, 'Кристофер Нолан', 1970),
-#     (3, 'Тим Бёртон', 1958)   
-# ]
+# cur.executescript('''
+# CREATE TABLE IF NOT EXISTS product_types(
+#     id INTEGER PRIMARY KEY,
+#     name TEXT NOT NULL
+# );
+# CREATE TABLE IF NOT EXISTS video_products2(
+#     id INTEGER PRIMARY KEY,
+#     name TEXT NOT NULL,
+#     type_id INTEGER NOT NULL,
+#     FOREIGN KEY(type_id) REFERENCES product_types(id)
+# );
+# ''')
 
-# video_products = [
-#     (3, 'Формула 1', 'Фильм', 2025),
-#     (4, 'И гаснет свет', 'Фильм', 2014),
-#     (5, 'Злое', 'Фильм', 2021)
-# ]
+video_products = [
+    (1, 'Безумные мелодии Луни Тюнз', 2),
+    (2, 'Веселые мелодии', 2),
+    (3, 'Кто подставил кролика Роджера', 3),
+    (4, 'Хороший, плохой, злой', 3),
+    (5, 'Последний киногерой', 3),
+    (6, 'Она написала убийство', 4),
+    (7, 'Миссис Харрис едет в Париж', 3),
+]
+
+product_types = [
+    (1, 'Мультфильм'),
+    (2, 'Мультсериал'),
+    (3, 'Фильм'),
+    (4, 'Сериал'),
+]
+
+# cur.executemany('INSERT INTO product_types VALUES(?, ?);', product_types)
+# cur.executemany('INSERT INTO video_products2 VALUES(?, ?, ?);', video_products)
+
+# results = cur.execute('''
+# SELECT video_products2.name, product_types.name 
+# FROM video_products2, product_types
+# WHERE video_products2.type_id = product_types.id AND product_types.name = "Фильм";
+# ''') 
+
+# for result in results:
+#     print(result)
+
+cur.executescript('''
+ALTER TABLE video_products2
+RENAME TO video_products;
+''')
+
+con.commit()
+con.close()
 
 # cur.executemany('INSERT INTO directors VALUES(?, ?, ?);', directors)
 # cur.executemany('INSERT INTO video_products VALUES(?, ?, ?, ?);', video_products)
